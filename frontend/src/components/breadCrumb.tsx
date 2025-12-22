14e55515e6ab1b1a436040867d2453a3ecee430e
ujjwal G <crystalninja0123@gmail.com>
2025-12-22 20:41:38 +0530

    kpi cards functional

--- frontend/src/components/breadCrumb.tsx
+++ frontend/src/components/breadCrumb.tsx
@@ -1,28 +1,60 @@
+import { Link, useLocation } from "react-router-dom";
 import {
   Breadcrumb,
-  BreadcrumbEllipsis,
   BreadcrumbItem,
   BreadcrumbLink,
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
+} from "../components/ui/breadcrumb";
+
 export function BreadcrumbDemo() {
+  const location = useLocation();
+
+  const segments = location.pathname.split("/").filter(Boolean);
+  const crumbs = segments.map((segment, index) => {
+    let path = "";
+    if (!segments.includes("market")) {
+      path = "/" + segments.slice(0, index + 1).join("/");
+    }
+    return {
+      label: formatLabel(segment),
+      path,
+      isLast: index === segments.length - 1,
+    };
+  });
+
   return (
     <Breadcrumb>
       <BreadcrumbList>
         <BreadcrumbItem>
-          <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          <BreadcrumbLink asChild>
+            <Link to="/">Home</Link>
+          </BreadcrumbLink>
         </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <BreadcrumbLink href="/components">Components</BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
-        </BreadcrumbItem>
+
+        {crumbs.length > 0 && <BreadcrumbSeparator />}
+
+        {crumbs.map((crumb, index) => (
+          <div key={crumb.path} className="flex items-center">
+            <BreadcrumbItem>
+              {crumb.isLast ? (
+                <BreadcrumbPage>{crumb.label}</BreadcrumbPage>
+              ) : (
+                <BreadcrumbLink asChild>
+                  <Link to={crumb.path}>{crumb.label}</Link>
+                </BreadcrumbLink>
+              )}
+            </BreadcrumbItem>
+
+            {!crumb.isLast && <BreadcrumbSeparator />}
+          </div>
+        ))}
       </BreadcrumbList>
     </Breadcrumb>
   );
+}
+
+function formatLabel(segment: string) {
+  return segment.replace(/-/g, " ").replace(/\b\w/g, (c) => c.toUpperCase());
 }
--- frontend/src/pages/homepage.tsx
+++ frontend/src/pages/homepage.tsx
@@ -3,64 +3,197 @@ import { Card } from "../components/ui/card";
 import { ChartAreaInteractive } from "../components/area-chart";
 import DemoPage from "../components/table/page";
 import { BarChart3, Users, Rocket, Mail } from "lucide-react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+interface KPIResponse {
+  cleanedData: {
+    pair: string;
+    from: string;
+    to: string;
+    rate: number;
+    bid: number;
+    ask: number;
+    lastUpdated: string;
+  };
+}
+function useCryptoPrice(coin: string) {
+  const [price, setPrice] = useState<number | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    let mounted = true;
+
+    async function fetchPrice() {
+      try {
+        const res = await axios.get("http://localhost:5000/api/crypto/kpi", {
+          params: { coin, CUR: "USD" },
+        });
+
+        if (mounted && res.data.cleanedData) {
+          setPrice(res.data.cleanedData.rate);
+        }
+      } catch (err) {
+        console.error(`${coin} price fetch failed`, err);
+      } finally {
+        mounted && setLoading(false);
+      }
+    }
+
+    fetchPrice();
+    return () => {
+      mounted = false;
+    };
+  }, [coin]);
+
+  return { price, loading };
+}
+
+// function useEthereumPrice(coin: string) {
+//   const [price, setPrice] = useState<number | null>(null);
+//   const [loading, setLoading] = useState(true);
+//
+//   useEffect(() => {
+//     let mounted = true;
+//
+//     async function fetchPrice() {
+//       try {
+//         const res = await axios.get("http://localhost:5000/api/crypto/finage", {
+//           params: { coin },
+//         });
+//
+//         if (mounted) {
+//           setPrice(res.data.cleanedData.price);
+//         }
+//       } catch (err) {
+//         console.error(`${coin} price fetch failed`, err);
+//       } finally {
+//         mounted && setLoading(false);
+//       }
+//     }
+//
+//     fetchPrice();
+//
+//     return () => {
+//       mounted = false;
+//     };
+//   }, [coin]);
+//
+//   return { price, loading };
+// }
+
+function useCurrencyPrice(curr: string) {
+  const [price, setPrice] = useState<number | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    let mounted = true;
+
+    async function fetchPrice() {
+      try {
+        const res = await axios.get("http://localhost:5000/api/crypto/curr", {
+          params: { to: curr },
+        });
+
+        if (mounted) {
+          setPrice(res.data.cleaned.to);
+        }
+      } catch (err) {
+        console.error(`${curr} price fetch failed`, err);
+      } finally {
+        mounted && setLoading(false);
+      }
+    }
+
+    fetchPrice();
+
+    return () => {
+      mounted = false;
+    };
+  }, [curr]);
+
+  return { price, loading };
+}
+function useMarketOpen() {
+  const [loading, setLoading] = useState(true);
+  const [marketOp, setMarket] = useState(false);
+
+  useEffect(() => {
+    let mounted = true;
+
+    async function fetchStatus() {
+      try {
+        const res = await axios.get("http://localhost:5000/api/market/status");
+
+        if (mounted) {
+          setMarket(res.data.open);
+        }
+        console.log(res, res.data.open);
+      } catch (err) {
+        console.error(`status fetch failed`, err);
+      } finally {
+        mounted && setLoading(false);
+      }
+    }
+
+    fetchStatus();
+
+    return () => {
+      mounted = false;
+    };
+  }, []);
+
+  return { marketOp, loading };
+}
 
 export default function HomePage() {
+  const { price: btcPrice } = useCryptoPrice("BTC");
+  // const { price: ethPrice } = useEthereumPrice("ethusd");
+
+  const { price: currencyprice } = useCurrencyPrice("usd");
+  const { marketOp: marketOpen } = useMarketOpen();
+
   return (
     <div className="space-y-6">
-      {/* KPI Card */}
+      {/* KPI Cards */}
       <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
         <KPICard
-          title="Market Assets"
-          value="2,431"
+          title="BTC Price"
+          value={btcPrice ? `$${btcPrice}` : "—"}
           icon={BarChart3}
-          stats={[
-            { label: "Crypto", value: 412 },
-            { label: "Stocks", value: 1720 },
-          ]}
         />
 
         <KPICard
-          title="Tracked Symbols"
-          value="102"
+          title="ETH Price"
+          // value={ethPrice ? `$${ethPrice}` : "—"}
           icon={Users}
-          stats={[
-            { label: "Active", value: 98 },
-            { label: "Inactive", value: 4 },
-          ]}
         />
 
         <KPICard
-          title="Alerts"
-          value="2"
+          title="EUR To USD"
+          value={"1EUR = " + currencyprice + "USD"}
           icon={Rocket}
-          stats={[
-            { label: "Triggered", value: 1 },
-            { label: "Pending", value: 1 },
-          ]}
         />
 
         <KPICard
-          title="API Requests"
-          value="1"
-          icon={Mail}
-          stats={[
-            { label: "Today", value: 1 },
-            { label: "Failed", value: 0 },
-          ]}
+          title="Market Status"
+          value={marketOpen ? "OPEN" : "CLOSED"}
+          icon={Rocket}
+          badge={marketOpen ? "Live" : "Closed"}
         />
       </div>
 
-      {/*  CHART */}
-      <Card className="w-full">
-        <ChartAreaInteractive />
+      {/* Chart */}
+      <Card>
+        <div className="px-4">
+          <ChartAreaInteractive />
+        </div>
       </Card>
 
-      {/* TABLES */}
+      {/* Tables */}
       <div className="grid grid-cols-1 gap-6 xl:grid-cols-2">
         <Card>
           <DemoPage />
         </Card>
-
         <Card>
           <DemoPage />
         </Card>
